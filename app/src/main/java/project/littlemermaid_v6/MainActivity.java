a86621f71d6b0a8746f768371997364fbe1d2ef9
hsu430 <hsu430@iastate.edu>
2016-12-04 06:10:28 -0600

    TODO - Retrieve

--- app/src/main/java/project/littlemermaid_v6/MainActivity.java
+++ app/src/main/java/project/littlemermaid_v6/MainActivity.java
@@ -1,57 +1,108 @@
 package project.littlemermaid_v6;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+        import android.content.Intent;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.TableLayout;
+        import android.widget.TableRow;
+        import android.widget.Toast;
 
-import com.firebase.client.ChildEventListener;
-import com.firebase.client.DataSnapshot;
-import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
-import com.firebase.client.GenericTypeIndicator;
+        import com.firebase.client.ChildEventListener;
+        import com.firebase.client.DataSnapshot;
+        import com.firebase.client.Firebase;
+        import com.firebase.client.FirebaseError;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+        import java.io.BufferedReader;
+        import java.io.FileNotFoundException;
+        import java.io.IOException;
+        import java.io.InputStreamReader;
+        import java.util.ArrayList;
+        import java.util.HashMap;
+        import java.util.HashSet;
+        import java.util.Iterator;
+        import java.util.Set;
 
-import static android.R.attr.id;
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
+        import static android.media.CamcorderProfile.get;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Firebase aRef;
+    //private Firebase aRef;
     private Button launchBtn;
-    private Button educationBtn, historyBtn, miscBtn;
 
-    /*Selection*/
-    ArrayList<Integer> cateSelect = new ArrayList<Integer>();
+    /*Display button topic*/
+    ArrayList<String> btnDisplay;
 
-    public String [] fName = { "File 1", "File 2", "Roflcopters","hahah"};
-    int index = 0;
+    /*Button Selection */
+    HashSet<String> setSelected;
+    //ArrayList<String> setSelected;
+
+    HashMap<String, Set<String>> topic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Hash map for topic
+        topic = new HashMap<String, Set<String>>();
+
+        //topic display on btn
+        btnDisplay = new ArrayList<String>();
+
+        //topic get selected
+        setSelected = new HashSet<>();
+        //setSelected = new ArrayList<>();
+
         //button get layout id
         launchBtn = (Button) findViewById(R.id.launchButton);
-        miscBtn = (Button) findViewById(R.id.miscBtn);
-        educationBtn = (Button) findViewById(R.id.educationBtn);
-        historyBtn = (Button) findViewById(R.id.historyBtn);
 
         //event handler
-        educationBtn.setOnClickListener(this);
-        historyBtn.setOnClickListener(this);
-        miscBtn.setOnClickListener(this);
         launchBtn.setOnClickListener(this);
 
+        //read file from csv
+        String line;
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(getAssets().open("Dec1606Lexicons.csv")));
+            while((line = br.readLine())!= null){
 
+                String[] words = line.split(",");
+
+                HashSet<String> set = new HashSet<>();
+                for(int i = 1 ; i < words.length ; i++){
+                    set.add(words[i]);
+                }
+                topic.put(words[0],set);
+
+                //save all the topic
+                //topic.put(words[0],set_name);
+                System.out.println(topic);
+                //System.out.println(words.length);
+            }
+
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        ////////////////////////////////////////////Store the key////////////////////////
+        String key = "";
+        //Get the set of key from hashmap
+        Set setOfKeys = topic.keySet();
+        //get the iterator instance from set
+        Iterator iterator = setOfKeys.iterator();
+        while (iterator.hasNext()) {
+            key = (String) iterator.next();
+            btnDisplay.add(key);
+            System.out.println("Key: "+ btnDisplay);
+        }
+        //////////////////////////////////////////////////////////////////////////////////////////
+
+        //display button
+        categoryDisplay();
     }
 
     @Override
@@ -59,54 +110,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         switch (view.getId()){
 
-            case R.id.educationBtn:
-                cateSelect.add(1);
-                break;
-            case R.id.historyBtn:
-                cateSelect.add(2);
-                break;
-            case R.id.miscBtn:
-                cateSelect.add(3);
-                break;
             case R.id.launchButton:
 
-                if(cateSelect.size() == 0){
-
+                if(setSelected.size() == 0){
                     Toast.makeText(getApplicationContext(), "Please selected category", Toast.LENGTH_SHORT).show();
-
                 }else {
-
                     //send the value to the receive
                     Intent intent = new Intent(MainActivity.this, ReceiveActivity.class);
-                    intent.putExtra("CateKey",cateSelect);
-
+                    intent.putExtra("Topic",setSelected);
                     //start send
                     startActivity(intent);
 
-                    //testing the index value
-                    for(int i = 0 ; i < cateSelect.size(); i++) {
-                        System.out.println("-Main activity--" + cateSelect.get(i));
-                    }
                     //reset the string
-                    cateSelect.clear();
-
+                    setSelected.clear();
                 }
 
                 break;
+
             default:
                 break;
         }
     }
 
-
-
-    /*
+    //Topic display
     private void categoryDisplay() {
-        try{
 
+        try{
             //create the 9 X 9 buttons on the table layout with correspond word
             for(int index = 0; index < 9; index++){
-                TableLayout table = (TableLayout) findViewById( R.id.buttonLayout );
+
+                TableLayout table = (TableLayout) findViewById( R.id.topicLayout);
 
                 int buttonsInRow = 0;
                 int numRows = table.getChildCount();
@@ -122,13 +155,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     buttonsInRow = 0;
                 }
                 if( buttonsInRow < 3 ){
-                    ToggleButton bb = new ToggleButton( this );
-                    row.addView( bb, 300, 200 );
-                    //bb.setText(Category [index]);
-                    bb.setText(Category [index]);
-                    bb.setTextOff(Category [index]);
-                    bb.setTextOn(Category [index]);
-                    //bb.setText(buttonString.get(index));
+                    Button bb = new Button( this );
+                    bb.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Button button = (Button) view;
+                            String selectedText = button.getText().toString();
+                            //user selected the button add to the array list
+                            if (topic.containsKey(selectedText)){
+                                setSelected.addAll(topic.get(selectedText));
+                                //System.out.println(topic.get(selectedText));
+                                System.out.println(setSelected.toString());
+                            }
+                            //Toast.makeText(getApplicationContext(), selectedText, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    row.addView( bb, 400, 200 );
+                    bb.setText(btnDisplay.get(index));
                 }
             }
 
@@ -136,23 +179,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         }
     }
-*/
+
 
 }
-
-
-            /*
-            aRef.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    Map<String, String>map = dataSnapshot.getValue(Map.class);
-                    String name = map.get("lemma");
-                    Log.v("The Value", "-------------Data I got:  " + name);
-                    //print all the object in the database
-                    //Log.v("The Value", "Data: " + dataSnapshot.getValue());
-                }
-                @Override
-                public void onCancelled(FirebaseError firebaseError) {
-                }
-            });
-            */
--- app/src/main/java/project/littlemermaid_v6/ReceiveActivity.java
+++ app/src/main/java/project/littlemermaid_v6/ReceiveActivity.java
@@ -29,6 +29,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -40,80 +41,33 @@ import static android.media.CamcorderProfile.get;
 public class ReceiveActivity extends Activity implements View.OnClickListener {
 
     private Button nextBtn, returnBtn;
-    private TextView test;
     private Firebase aRef;
 
-    private static final int ROWS = 2;
-    private static final int COLS = 3;
-
-    ArrayList<Integer> cateSelected = new ArrayList<Integer>();
-
-    // Format Store ---- < Lemma , Category : Key >
-    HashMap<String, Integer> lemmaKey = new HashMap<String, Integer>();
-
-    // Format Store ---- < Category , < Lemma , Category : Key > >
-    HashMap<String, HashMap<String, Integer>> CateLemmaKey = new HashMap<String, HashMap<String, Integer>>();
-
-    public String [] fName = { "File 1", "File 2", "Roflcopters","fun","show","File 4","File 5","File 6"};
-    public int indexPoint = 0;
-    GridView gv;
-
+    /*Button Selection from launch activity*/
+    HashSet<String> setSelected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive);
 
-        //gv = (GridView) findViewById(R.id.gv);
         nextBtn = (Button)findViewById(R.id.nextBtn);
+        GridView gv = (GridView) findViewById(R.id.gv);
 
-        for(int index = 0; index < fName.length; index++){
-            TableLayout table = (TableLayout) findViewById( R.id.btn);
+        //topic get selected
+        setSelected = new HashSet<>();
 
-            int buttonsInRow = 0;
-            int numRows = table.getChildCount();
-            TableRow row = null;
-            if( numRows > 0 ){
-                row = (TableRow) table.getChildAt( numRows - 1 );
-                buttonsInRow = row.getChildCount();
-            }
-
-            if( numRows == 0 || buttonsInRow == 2 ){
-                row = new TableRow( this );
-                table.addView( row );
-                buttonsInRow = 0;
-            }
-            if( buttonsInRow < 2 ){
-                Button bb = new Button( this );
-                row.addView( bb, 500, 200 );
-                //bb.setText("0");
-                bb.setText(fName[index]);
-            }
-        }
-
-
-
-
-
-        //GridView gridview = (GridView) findViewById(R.id.gv);
-        //gridview.setAdapter(new ButtonAdapter(this));
-
-
-       // dynamicBtnDisplay();
-
-        /*
-        test = (TextView) findViewById(R.id.test);
-
+        /* Get the user selected value from category page*/
         Intent intent = getIntent();
-        cateSelected = intent.getIntegerArrayListExtra("CateKey");
-        System.out.println("-From other activity---" + cateSelected);
-
+        setSelected = (HashSet<String>) intent.getSerializableExtra("Topic");
+        System.out.println("-From other activity---" + setSelected);
 
         nextBtn = (Button) findViewById(R.id.nextBtn);
         returnBtn = (Button) findViewById(R.id.returnBtn);
         nextBtn.setOnClickListener(this);
         returnBtn.setOnClickListener(this);
 
+        //connect to the Firebase
         aRef = new Firebase("https://littlemermaid.firebaseio.com/dictionary/");
 
         aRef.addChildEventListener(new ChildEventListener() {
@@ -122,94 +76,33 @@ public class ReceiveActivity extends Activity implements View.OnClickListener {
 
                 GenericTypeIndicator<Map<String, Object>> m = new GenericTypeIndicator<Map<String, Object>>(){};
                 Map<String, Object> map = dataSnapshot.getValue(m);
-
                 //word
-                String lemma = (String) map.get("lemma");
-
-                //pull out the user selected
-                for(int i = 0; i<cateSelected.size();i++){
-
-                    int number = cateSelected.get(i);
-                    if(number == 1){
-                        int Education = (int) map.get("ACAD:Education");
-                        //remove zero value
-                        if(Education != 0){
-                            lemmaKey.put(lemma,Education);
-                            //CateLemmaKey.put("Education",lemmaKey);
-                            //Log.v("Education---", lemmaKey.toString());
-                        }
-                    }else if(number == 2){
-                        int History = (int) map.get("ACAD:History");
-                        //remove zero value
-                        if(History != 0){
-                            lemmaKey.put(lemma,History);
-                            //CateLemmaKey.put("History",lemmaKey);
-                        }
-                    }else if(number == 3){
-                        int Misc = (int) map.get("ACAD:Misc");
-                        //remove zero value
-                        if(Misc != 0){
-                            lemmaKey.put(lemma,Misc);
-                            //CateLemmaKey.put("Misc",lemmaKey);
-                        }
-                    }
-                }
-
+                String lemma = (String) map.get(setSelected);
+                System.out.println(lemma);
+                //check each set of value
+                /*
+                                for (String setVal : setSelected) {
+                                    System.out.println(setVal);
+                                }*/
             }
 
             @Override
             public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
             }
-
             @Override
             public void onChildRemoved(DataSnapshot dataSnapshot) {
-
             }
-
             @Override
             public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
             }
-
             @Override
             public void onCancelled(FirebaseError firebaseError) {
-
             }
 
         });
-*/
-    }
-
-    /*
-    private void dynamicBtnDisplay(){
-
-        TableLayout table = (TableLayout) findViewById(R.id.btnLayout);
-        for(int row = 0; row < ROWS ;row++){
-            TableRow tableRow = new TableRow(this);
-
-            table.addView(tableRow);
-            for(int col = 0;col < COLS ;col++){
-                Button button = new Button(this);
-
-                button.setText(text.get(1));
-                button.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        btnClick();
-                    }
-                });
-
-                tableRow.addView(button);
-            }
-
-        }
-    }
-
-    private void btnClick(){
 
     }
-*/
+
 
     /*
     private void dynamicBtnDisplay(int i, int size) {
